6e7c810838a08274460960f07a6e98552bff0389
TheSteelCrow <127387250+TheSteelCrow@users.noreply.github.com>
2024-06-24 22:04:55 +1200

    6 24 2024 - part 2
    Added search engine
    Added date and time
    Added more backgrounds
    Added taskbar buttons

---
+++ Scenes/SearchEngine.gd
@@ -0,0 +1,39 @@
+extends Panel
+
+var SearchEngineOpen = false
+
+var Mottos = [
+	"Parcourir: Where Your Data Finds a New Home.",
+	"Parcourir: Your Privacy? What Privacy?",
+	"Parcourir: Surf and Surrender Your Secrets.",
+	"Parcourir: We Know What You Did Last Session.",
+	"Parcourir: The Browser That Keeps a Little for Itself.",
+	"Parcourir: Trust Us... Maybe You Shouldn't.",
+	"Parcourir: Because Your Data is Our Business.",
+	"Parcourir: Browse Boldly, Share Unknowingly.",
+	"Parcourir: The Browser with a Hidden Agenda.",
+	"Parcourir: Your Window to the Web, Our Door to Your Data."
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func ResetSearchBrowserText():
+	get_node("Motto").text = "[center]" + Mottos[randf_range(0, Mottos.size())] + "[/center]"
+
+func _on_search_engine_button_button_up():
+	print("Search Engine Button Pressed")
+	if(SearchEngineOpen):
+		SearchEngineOpen = !SearchEngineOpen
+		hide()
+	else:
+		SearchEngineOpen = !SearchEngineOpen
+		show()
+		ResetSearchBrowserText()
--- Scripts/Taskbar.gd
+++ Scripts/Taskbar.gd
@@ -1,5 +1,7 @@
 extends Node
 
+
+
 var MenuPanel
 var MenuOpen = false
 
@@ -7,10 +9,25 @@ var MenuOpen = false
 func _ready():
 	MenuPanel = get_parent().get_node("Menu")
 	MenuPanel.hide()
+	var time = Time.get_time_dict_from_system()
+	print(time)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var TimeText = get_node("TimeText")
+	var DateText = get_node("DateText")
+	TimeText.text = ""
+	var SystemTime = Time.get_time_dict_from_system()
+	var SystemDate = Time.get_date_dict_from_system()
+	if(SystemTime.hour > 12):
+		TimeText.text = "[right]" + str(SystemTime.hour - 12) + ":" + str(SystemTime.minute) + " pm" + "[/right]"
+	else:
+		TimeText.text = "[right]" + str(SystemTime.hour) + ":" + str(SystemTime.minute) + " am" + "[/right]"
+		
+	if(SystemDate.month < 10):
+		DateText.text = "[right]" + str(SystemDate.day) + "/0" + str(SystemDate.month) + "/" + str(SystemDate.year) + "[/right]"
+	else:
+		DateText.text = "[right]" + str(SystemDate.day) + "/" + str(SystemDate.month) + "/" + str(SystemDate.year) + "[/right]"
 
 func _on_menu_button_button_up():
 	print("Menu Button Pressed")
