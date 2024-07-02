b4ea8ee583fda029b5dfc1eea4dfacdc623a178b
22lotdan <22lotdan@gmail.com>
2024-07-02 12:28:37 +1200

    7 2 2024
    Fixed Errors Multi Dragging

--- Scripts/ErrorPopup.gd
+++ Scripts/ErrorPopup.gd
@@ -5,8 +5,11 @@ var MouseInZone = false
 var Dragging = false
 var offset = Vector2(0,0)
 var ScreenSize 
+var Main
 
 func _ready():
+	Main = get_parent()
+	print(Main.WindowInstanceSelected)
 	ScreenSize = get_viewport_rect().size / get_canvas_transform().get_scale()
 	#position = Vector2(ScreenSize.x/2 - size.x/2, ScreenSize.y/2 - size.y/2)
 	position = Vector2(randf_range(0, ScreenSize.x - size.x), randf_range(0, ScreenSize.y - size.y))
@@ -17,16 +20,19 @@ func _process(delta):
 	MouseButton1Down = Input.is_mouse_button_pressed(1)
 	var MousePosition = get_viewport().get_mouse_position()
 	
-	if(MouseInZone && MouseButton1Down && Dragging == false):
+	if(MouseInZone && MouseButton1Down && Dragging == false && Main.WindowInstanceSelected == false):
 		Dragging = true
 		offset = position - MousePosition
+		Main.WindowSelected()
 		#Set Offset
 	elif(MouseButton1Down == false && Dragging == true):
 		Dragging = false
+		Main.WindowDeselected()
 	elif(Dragging == true):
 		if(not (MousePosition.x > ScreenSize.x || MousePosition.x < 0 || MousePosition.y > ScreenSize.y || MousePosition.y < 0 )):
 			position = get_viewport().get_mouse_position() + offset
-		#MoveWindow
+			move_to_front()
+			#MoveWindow
 	
 
 func _on_file_explorer_button_button_up():
@@ -37,9 +43,14 @@ func _on_file_explorer_button_button_up():
 		show()
 		move_to_front()
 
-
 func _on_panel_mouse_entered():
 	MouseInZone = true
 	
 func _on_panel_mouse_exited():
 	MouseInZone = false
+
+func _on_x_button_button_up():
+	queue_free()
+
+func _on_close_button_button_up():
+	queue_free()
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -5,7 +5,15 @@ var Menu
 var FileExplorer
 var ErrorPopup
 
+var WindowInstanceSelected = false
+
+var Cursor1 = load("res://Images/pointer_b.png")
+var Cursor2 = load("res://Images/pointer_b_shaded.png")
+
 func _ready():
+	#OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	Input.set_custom_mouse_cursor(Cursor1, Input.CURSOR_ARROW)
+	Input.set_custom_mouse_cursor(Cursor2, Input.CURSOR_POINTING_HAND)
 	ErrorPopup = preload("res://Prefabs/error_popup.tscn")
 	SpawnError()
 	Menu = get_node("Menu")
@@ -29,3 +37,12 @@ func SpawnError():
 
 func _on_error_spawner_timeout():
 	SpawnError()
+	
+func WindowSelected():
+	if(WindowInstanceSelected == false):
+		WindowInstanceSelected = true
+	else:
+		print("A window has already been selected")
+	
+func WindowDeselected():
+	WindowInstanceSelected = false
