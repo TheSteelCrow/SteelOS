89b1abdb13f4edd18daf48878bb7aa2155139d80
TheSteelCrow <127387250+TheSteelCrow@users.noreply.github.com>
2024-07-17 18:50:03 +1200

    17 07 24 - part 2
    brought character renderer into acceptable state but it needs more work

--- Scripts/character_renderer.gd
+++ Scripts/character_renderer.gd
@@ -23,16 +23,17 @@ func _on_button_button_up():
 	elif(SexBinary == 1):
 		SexName = "Woman"
 		
-	Info.text += "\nSex = " + SexName
+	Info.text += "\nSex: " + SexName
 	
 	for BodyPart in get_children():
-		if(BodyPart.name.contains("Shirt")):
+		if(BodyPart.name.contains("Shirt") or BodyPart.name.contains("Hair") or BodyPart.name.contains("Pant")):
 			BodyPart.hide()
 	
 	#Choose Face
 	get_node("Face").frame = randi() % 4
 	#Choose Skin Tone
 	var SkinTone = randi() % 8
+	Info.text += "\nSkin Tone: " + str(SkinTone+1) + "/" + "8"
 	get_node("Head").frame = SkinTone
 	get_node("Neck").frame = SkinTone
 	get_node("ArmLeft").frame = SkinTone
@@ -66,27 +67,83 @@ func _on_button_button_up():
 	RightSleeve.frame = ShirtColour
 	# ^ This stuff aint workin cuz I removed the breast ones fix that pls, im too tired
 	
+	#Pants
+	
+	random.randomize()
+	var PantsColour = random.randi_range(0, 11)
+	random.randomize()
+	var PantsType = random.randi_range(0, 2)
+	random.randomize()
+	var StripType = random.randi_range(0, 3)
+	var PantsTypeName
+	
+	var LeftPant
+	var RightPant
+	
+	if(PantsType == 0):
+		PantsTypeName = "Short"
+	elif(PantsType == 1):
+		PantsTypeName = "Medium"
+	elif(PantsType == 2):
+		PantsTypeName = "Long"
+	
+	LeftPant = get_node("Pants" + PantsTypeName + "Left")
+	RightPant = get_node("Pants" + PantsTypeName + "Right")
+	
+	LeftPant.show()
+	RightPant.show()
+	
+	LeftPant.frame = PantsColour
+	RightPant.frame = PantsColour
+	
+	var PantsMiddle = get_node("PantsMiddle")
+	PantsMiddle.show()
+	
+	PantsMiddle.frame = StripType  + (4 * PantsColour)
+	
+	#Shirt Middle
+	
 	var ShirtMiddle = get_node("ShirtMiddleMan")
 	ShirtMiddle.show()
 	ShirtMiddle.frame = (randi() % 8) + (8 * ShirtColour)
 	
 	Info.text += "\nAge: " + str(Age)
 	var Old
-	
-	var Hair = get_node("HairMan")
-	
-	Hair.hide()
+	var Hair
 	
 	print(Age)
 	if(Age >= 50):
-		Hair.show()
+		
 		Old = true
-		Info.text += "\nIsOld = true"
+		Info.text += "\nIsOld: yes"
 		random.randomize()
-		Hair.frame = random.randi_range(49, 63)
-	else:
+		
+		if(SexName == "Man"):
+			Hair = get_node("HairMan")
+			Hair.frame = random.randi_range(49, 63)
+		elif(SexName == "Woman"):
+			Hair = get_node("HairWoman")
+			Hair.frame = random.randi_range(30, 47)
+			
 		Hair.show()
+	elif(Age < 50):
+		
 		Old = false
-		Info.text += "\nIsOld = false"
+		Info.text += "\nIsOld: no"
 		random.randomize()
-		Hair.frame = random.randi_range(0, 48)
+		
+		if(SexName == "Man"):
+			Hair = get_node("HairMan")
+			Hair.frame = random.randi_range(0, 48)
+		elif(SexName == "Woman"):
+			Hair = get_node("HairWoman")
+			Hair.frame = random.randi_range(0, 29)
+		
+		Hair.show()
+	
+	var ShoeLeft = get_node("ShoeLeft")
+	var ShoeRight = get_node("ShoeRight")
+	random.randomize()
+	var ShoeColour = random.randi_range(0, 34)
+	ShoeLeft.frame = ShoeColour
+	ShoeRight.frame = ShoeColour
