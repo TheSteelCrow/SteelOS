6893cd6d1aa38a3545cc7fc92b5a93fd7c7c66c2
TheSteelCrow <127387250+TheSteelCrow@users.noreply.github.com>
2024-07-25 21:10:38 +1200

    25 07 24
    Redesigned hire and fire activities

--- Scripts/TeamManager.gd
+++ Scripts/TeamManager.gd
@@ -7,8 +7,6 @@ var NewCharacterRenderer
 var Character1
 var Character2
 
-var ActivityType = "Fire"
-
 var ApproximationWords = ["roughly", "around", "nearly", "just over", "approximately", "almost", "just under"]
 var QualityWords = ["good", "good", "good", "good", "decent", "decent", "alright", "bad", "fun", "smart", "intelligent", "hard working", "great", "average", "average", "average", "average"]
 
@@ -18,20 +16,24 @@ var CanInteract = true
 
 var Taskbar
 
+var StampOriginPosition
+var DraggingStamp = false
+var StampOffSetFromMouse
+var Stamp
+var StampTimeUntilReturn = 1
+
 func SetupRound():
 	RemoveOldCharacters()
 	CanInteract = true
 	
+	
+	
 	if(Taskbar.TaskName == "Fire"):
-		get_node("BottomPanel1").get_node("1FireButton").show()
-		get_node("BottomPanel2").get_node("2FireButton").show()
-		get_node("BottomPanel1").get_node("1HireButton").hide()
-		get_node("BottomPanel2").get_node("2HireButton").hide()
+		get_node("Stamp/Fire").show()
+		get_node("Stamp/Hire").hide()
 	elif(Taskbar.TaskName == "Hire"):
-		get_node("BottomPanel1").get_node("1HireButton").show()
-		get_node("BottomPanel2").get_node("2HireButton").show()
-		get_node("BottomPanel1").get_node("1FireButton").hide()
-		get_node("BottomPanel2").get_node("2FireButton").hide()
+		get_node("Stamp/Fire").hide()
+		get_node("Stamp/Hire").show()
 	
 	NewCharacterRenderer = preload("res://Prefabs/new_character_renderer.tscn")
 	
@@ -39,22 +41,24 @@ func SetupRound():
 	Character1.name = "Character1"
 	add_child(Character1)
 	move_child(Character1, 1)
-	Character1.position = Vector2(64,64)
+	Character1.position = Vector2(52,98)
 	Character1.Generate()
 	GenerateInfo("1")
-	
+	Character1.move_to_front()
 	
 	Character2 = NewCharacterRenderer.instantiate()
 	Character2.name = "Character2"
 	add_child(Character2)
 	move_child(Character2, 1)
-	Character2.position = Vector2(992,64)
+	Character2.position = Vector2(1184, 102)
 	Character2.Generate()
 	GenerateInfo("2")
+	Character2.move_to_front()
 	
+	get_node("Warning").move_to_front()
 
 func GenerateInfo(CharacterNumber):
-	var InfoOutput = get_node("BottomPanel" + CharacterNumber).get_node("Info")
+	var InfoOutput = get_node("Employee" + CharacterNumber).get_node("Info")
 	var InfoInput = get_node("Character" + CharacterNumber)
 	
 	var InfoUnformated = "%s is a%s %s employee who has worked at the company for %s %s year%s. %s is %s and %s %s child%s."
@@ -113,15 +117,45 @@ func Open():
 
 func _ready():
 	get_node("Warning").show()
-	
+	get_node("Warning").move_to_front()
 	Taskbar = get_parent().get_node("Taskbar")
 	
-	var HireFireButtons = get_node("BottomPanel1").get_children()
-	HireFireButtons.append_array(get_node("BottomPanel2").get_children())
+	Stamp = get_node("Stamp")
+	StampOriginPosition = Stamp.position
 	
-	for HireFireButton in HireFireButtons:
-		if(HireFireButton.name.contains("Button")):
-			HireFireButton.button_up.connect(func():HireFireButtonUp(HireFireButton))
+	Stamp.get_node("Button").button_down.connect(func():
+		DraggingStamp = true
+		print("ButtonDown")
+		StampOffSetFromMouse = Stamp.position - get_viewport().get_mouse_position()
+	)
+	Stamp.get_node("Button").button_up.connect(func():
+		DraggingStamp = false
+		Stamp.get_node("StampSFX").play()
+		
+		Stamp.get_node("Button").disabled = true
+		var StampMovingPart = Stamp.get_node(Taskbar.TaskName)
+		var tween = create_tween()
+		tween.tween_property(StampMovingPart, "position", Vector2(StampMovingPart.position.x, StampMovingPart.position.y + 50), 0.1)
+		await tween.finished
+		var StampImageInk = get_node(Taskbar.TaskName + "d")
+		StampImageInk.position = Vector2(148,329)
+		StampImageInk.show()
+		StampImageInk.move_to_front()
+		Stamp.move_to_front()
+		get_node("Warning").move_to_front()
+		var tween2 = create_tween()
+		tween2.tween_property(StampMovingPart, "position", Vector2(StampMovingPart.position.x, StampMovingPart.position.y - 50), 0.25)
+		await tween2.finished
+		await get_tree().create_timer(StampTimeUntilReturn).timeout
+		get_node("NextButton").show()
+		Stamp.position = StampOriginPosition
+	)
+
+func _process(delta):
+	if(DraggingStamp):
+		Stamp.position = get_viewport().get_mouse_position() + StampOffSetFromMouse
+		Stamp.move_to_front()
+		get_node("Warning").move_to_front()
 
 func OnAppVisible():
 	Reset()
