984276831392bdb252d03b1633554ee90ca814cd
22lotdan <22lotdan@gmail.com>
2024-07-29 15:16:03 +1200

    7 29 24
    continued search engine

--- Scripts/parcourir.gd
+++ Scripts/parcourir.gd
@@ -1,10 +1,31 @@
 extends Panel
 
-# Called when the node enters the scene tree for the first time.
+var SearchResultPrefab
+var NumberOfSearchResults = 15
+var NumberOfSearchResultsCreated = 0
+var SearchResultsHolder
+
+@onready var SearchResults = $SearchResults
+
 func _ready():
-	pass # Replace with function body.
+	SearchResults.hide()
+	SearchResultPrefab = preload("res://Prefabs/search_result.tscn")
+	SearchResultsHolder = SearchResults.get_node("ScrollContainer/SearchResultsHolder")
+	DisplaySearchResults()
 
+func DisplaySearchResults():
+	SearchResults.show()
+	for i in range(0, NumberOfSearchResults):
+		NumberOfSearchResultsCreated += 1
+		print(NumberOfSearchResultsCreated)
+		var NewSearchResult = SearchResultPrefab.instantiate()
+		SearchResultsHolder.add_child(NewSearchResult)
+		NewSearchResult.get_node("Name").text = str(i)
+	SearchResultsHolder.move_child(SearchResultsHolder.get_node("EndingSeperator"), SearchResultsHolder.get_children().size())
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_local_user_search_input_text_submitted(new_text):
+	DisplaySearchResults()
+
+func _on_search_button_on_bar_pressed():
+	print("adam est null")
+	#DisplaySearchResults()
