71bbadf8980a27a8abaf80b8bba6c075549caf1e
TheSteelCrow <127387250+TheSteelCrow@users.noreply.github.com>
2024-07-29 20:59:46 +1200

    Backup

--- Scripts/parcourir.gd
+++ Scripts/parcourir.gd
@@ -1,31 +1,45 @@
 extends Panel
 
 var SearchResultPrefab
-var NumberOfSearchResults = 15
-var NumberOfSearchResultsCreated = 0
 var SearchResultsHolder
 
 @onready var SearchResults = $SearchResults
+
+#Name : Link, FormalName, Description
+
+var SearchEngine
+
+var WebDomains = {
+	
+	"parcourir" : ["www.parcourir.com", "Parcourir", "Parcourir is a search engine designed to search the web. Parcourir means browse or search."],
+	"government" : ["www.aloftia.govt", "Aloftia Government Login", "Login terminal for the Aloftia Government"]
+	
+}
 
 func _ready():
 	SearchResults.hide()
 	SearchResultPrefab = preload("res://Prefabs/search_result.tscn")
 	SearchResultsHolder = SearchResults.get_node("ScrollContainer/SearchResultsHolder")
-	DisplaySearchResults()
 
 func DisplaySearchResults():
 	SearchResults.show()
-	for i in range(0, NumberOfSearchResults):
-		NumberOfSearchResultsCreated += 1
-		print(NumberOfSearchResultsCreated)
+	for Domain in WebDomains:
 		var NewSearchResult = SearchResultPrefab.instantiate()
 		SearchResultsHolder.add_child(NewSearchResult)
-		NewSearchResult.get_node("Name").text = str(i)
+		
+		#NewSearchResult.button_up.connect(func():
+			
+		#)
+		
+		NewSearchResult.get_node("Name").text = Domain
+		NewSearchResult.get_node("Link").text = WebDomains[Domain][0]
+		NewSearchResult.get_node("FormalName").text = WebDomains[Domain][1]
+		NewSearchResult.get_node("Description").text = WebDomains[Domain][2]
+		
 	SearchResultsHolder.move_child(SearchResultsHolder.get_node("EndingSeperator"), SearchResultsHolder.get_children().size())
 
 func _on_local_user_search_input_text_submitted(new_text):
 	DisplaySearchResults()
 
 func _on_search_button_on_bar_pressed():
-	print("adam est null")
-	#DisplaySearchResults()
+	DisplaySearchResults()
