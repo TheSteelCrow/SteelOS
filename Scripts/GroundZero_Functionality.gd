6e7bfdf66f049d7bf29607486a01a0974677218a
TheSteelCrow <127387250+TheSteelCrow@users.noreply.github.com>
2024-08-11 22:46:33 +1200

    11 08 24 part 2
    continued ground zero

---
+++ Scripts/GroundZero_Functionality.gd
@@ -0,0 +1,16 @@
+extends Control
+
+var App
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	App = get_parent()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_scram_button_up():
+	App.Cutout.show()
